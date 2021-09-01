d4fef50f6691dbd06981f8ccb18b7d78f3f87754
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-09-01 18:58:19 -0400

    inital portfolio structure

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Typography, Grid, makeStyles } from "@material-ui/core";
 
-function Portfolio() {
-    return (
-        <div></div>
-    );
+const useStyles = makeStyles(() => ({
+    titleStyle: {
+      fontFamily: "Noto Sans JP, sans-serif",
+      fontWeight: 900,
+      textAlign: "center",
+      padding: "50px 0px 5px 0px",
+      marginBottom: "10px",
+    },
+    borderStyles: {
+      borderBottom: "6px solid #15d803",
+      width: "8%",
+      margin: "0 auto 50px auto",
+    }
+
+  }));
+
+export default function Portfolio() {
+
+  const {
+    titleStyle,
+    borderStyles,
+  } = useStyles();
+
+  return (
+    <div className="portfolioContainer" id="portfolio">
+      <Typography variant="h3" className={titleStyle}>
+        PORTFOLIO
+      </Typography>
+      <div className={borderStyles}></div>
+
+
+      <Grid container>
+        <Grid item md={4}>
+          <Item>{aboutCard}</Item>
+        </Grid>
+
+        <Grid item md={6} className={skillStyle} style={{ width: "100%" }}>
+          <SkillBar skills={skills} colors={colors} className={skillBar} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-export default Portfolio;
